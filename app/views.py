901599535b0d14597f21e9701be5453323ab8140
Liu Jackson <liuzhigang@huaqin.com>
2015-07-15 18:58:55 +0800

    form

--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
 #!flask/bin/python
 
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from .forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -23,9 +24,14 @@ def index():
                            user=user,
                            posts=posts)
 
-@app.route('/login', methods = ['GET', 'POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data +
+              '", remember_me=' + str(form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html',
-        title = 'Sign In',
-        form = form)
+                           title='Sign In',
+                           form=form)
